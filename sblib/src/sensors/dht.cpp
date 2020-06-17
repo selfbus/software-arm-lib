507151bcbf977603bff3716bc99e1982e9e25a57
Mario Theodoridis <mario@schmut.com>
2020-06-17 19:13:51 +0200

    remnove raw stuff again, make dht code more readable and tweak timing

--- sblib/src/sensors/dht.cpp
+++ sblib/src/sensors/dht.cpp
@@ -53,59 +53,100 @@ bool DHT::readData(bool bForceRead)
   bool bRet= false;
   this->_lastError= ERROR_NONE;
   int currenttime = millis();
-  if (!bForceRead && ((currenttime - this->_lastReadTime) < 2000)) this->_lastError= ERROR_TIMER_NOT_REACHED;
-  else
-  {
-    uint8_t cycles[80];
-    this->_lastReadTime = currenttime;
-    this->DHT_data[0]=this->DHT_data[1]=this->DHT_data[2]=this->DHT_data[3]=this->DHT_data[4]=0; // Reset 40 bits of received data to zero.
-    digitalWrite(_pin, 1);                                 // High impedence state to let pull-up raise data line level and start the reading process.
-    delay(250);
-    pinMode(_pin, OUTPUT);                                 // First set data line low for 20 milliseconds.
-    digitalWrite(_pin, 0);
-    delay(20);
+  if (!bForceRead && ((currenttime - this->_lastReadTime) < 2000)) {
+    this->_lastError= ERROR_TIMER_NOT_REACHED;
+    return bRet;
+  }
 
-    pinDisableInterrupt(_pin);                             // Turn off interrupts temporarily. This section timings are critical.
-    digitalWrite(_pin, 1);                                 // End the start signal by setting data line high for 40 microseconds.
-    delayMicroseconds(40);
-    pinMode(_pin, INPUT| PULL_UP);                         // Now start reading the data line to get the value from the DHT sensor.
-    delayMicroseconds(10);                                 // Delay a bit to let sensor pull data line low.
-    if(this->expectPulse(0) == 0) this->_lastError= ERROR_DATA_TIMEOUT;// First expect a low signal for ~80 microseconds followed by a high signal for ~80 microseconds again.
-    else {
-      if (this->expectPulse(1) == 0) this->_lastError= ERROR_NOT_PRESENT;
-      else {
-        for(uint8_t i=0; i<80; i+=2) {
-          cycles[i]   = this->expectPulse(0);
-          cycles[i+1] = this->expectPulse(1);
-        }
-        for(uint8_t i=0; i<40 && this->_lastError==ERROR_NONE; ++i){// Inspect pulses and determine which ones are 0 (high state cycle count < low
-          uint8_t lowCycles  = cycles[2*i];                // state cycle count), or 1 (high state cycle count > low state cycle count).
-          uint8_t highCycles = cycles[2*i+1];
-          if((lowCycles == 0) || (highCycles == 0)) {
-            this->_lastError= ERROR_DATA_TIMEOUT;
-          } else {
-            this->DHT_data[i/8] <<= 1;                     // Now compare the low and high cycle times to see if the bit is a 0 or 1.
-            if(highCycles > lowCycles) this->DHT_data[i/8] |= 1;// High cycles are greater than 50us low cycle count, must be a 1.
-          }
-        }
+  uint8_t cycles[80];
+  this->_lastReadTime = currenttime;
+  // Reset 40 bits of received data to zero.
+  this->DHT_data[0]=this->DHT_data[1]=this->DHT_data[2]=this->DHT_data[3]=this->DHT_data[4]=0;
+  // High impedance state to let pull-up raise data line level and start the reading process.
+  digitalWrite(_pin, 1);
+  delay(1);
+  // First set data line low for 20 milliseconds.
+  pinMode(_pin, OUTPUT);
+  digitalWrite(_pin, 0);
+  if (this->_DHTtype == DHT22) {
+    delayMicroseconds(1100);
+  } else {
+    delay(20);
+  }
+
+  do {
+    // Turn off interrupts temporarily. This section timings are critical.
+    pinDisableInterrupt(_pin);
+    // End the start signal by setting data line high for 40 microseconds.
+    digitalWrite(_pin, 1);
+    delayMicroseconds(55);
+    // Now start reading the data line to get the value from the DHT sensor.
+    pinMode(_pin, INPUT| PULL_UP);
+    // Delay a bit to let sensor pull data line low.
+    delayMicroseconds(10);
+
+    if (this->expectPulse(0) == 0) {
+      // First expect a low signal for ~80 microseconds followed by a high signal for ~80 microseconds again.
+      this->_lastError = ERROR_DATA_TIMEOUT;
+      break;
+    }
+
+    if (this->expectPulse(1) == 0) {
+      this->_lastError = ERROR_NOT_PRESENT;
+      break;
+    }
+
+    for (uint8_t i = 0; i < 80; i += 2) {
+      cycles[i] = this->expectPulse(0);
+      cycles[i + 1] = this->expectPulse(1);
+    }
+    // Inspect pulses and determine which ones are 0 (high state cycle count < low
+    for (uint8_t i = 0; i < 40 && this->_lastError == ERROR_NONE; ++i) {
+      // state cycle count), or 1 (high state cycle count > low state cycle count).
+      uint8_t lowCycles = cycles[2 * i];
+      uint8_t highCycles = cycles[2 * i + 1];
+      if ((lowCycles == 0) || (highCycles == 0)) {
+        this->_lastError = ERROR_DATA_TIMEOUT;
+        break;
+      }
+      // Now compare the low and high cycle times to see if the bit is a 0 or 1.
+      this->DHT_data[i / 8] <<= 1;
+      if (highCycles > lowCycles) {
+        // High cycles are greater than 50us low cycle count, must be a 1.
+        this->DHT_data[i / 8] |= 1;
       }
     }
-    pinEnableInterrupt(_pin);                              // Turn on pin interrupts. Timing critical code is now complete.
-    if(this->_lastError == ERROR_NONE) {                   // Check we read 40 bits and that the checksum matches.
-      if(this->DHT_data[4] == ((this->DHT_data[0] + this->DHT_data[1] + this->DHT_data[2] + this->DHT_data[3]) & 0xFF))
-      {
-        _lastTemperature= (_DHTtype == DHT22) ? (this->DHT_data[2] & 0x80) ? (((0x7F & this->DHT_data[2])<<8) +
-                          this->DHT_data[3])*10 : ((this->DHT_data[2]<<8) + this->DHT_data[3])*10 :
-                          (this->DHT_data[2] & 0x80) ? ((0x7F & this->DHT_data[2]) *-100) : this->DHT_data[2]*100;
-        _lastHumidity= (_DHTtype ==  DHT22) ? ((this->DHT_data[0]<<8) + this->DHT_data[1])*10 : this->DHT_data[0]*100;
+  } while(0);
+  // Turn on pin interrupts. Timing critical code is now complete.
+  pinEnableInterrupt(_pin);
 
-        _lastTemperature= _lastTemperature/100;
-        _lastHumidity= _lastHumidity/100;
+  // Check we read 40 bits and that the checksum matches.
+  if(this->_lastError != ERROR_NONE) return bRet;
 
-        bRet= true;
-      } else this->_lastError = ERROR_CHECKSUM;
-    }
+  if(this->DHT_data[4] != ((unsigned)(this->DHT_data[0] + this->DHT_data[1] + this->DHT_data[2] + this->DHT_data[3]) & 0xFF))
+  {
+    this->_lastError = ERROR_CHECKSUM;
+    return bRet;
   }
+  if (this->_DHTtype == DHT22) {
+    if (this->DHT_data[2] & 0x80) {
+      this->_lastTemperature = (((0x7F & this->DHT_data[2])<<8) + this->DHT_data[3])*-10;
+    } else {
+      this->_lastTemperature = ((this->DHT_data[2]<<8) + this->DHT_data[3])*10;
+    }
+    this->_lastHumidity= ((this->DHT_data[0]<<8) + this->DHT_data[1])*10;
+  } else {
+    if (this->DHT_data[2] & 0x80) {
+      this->_lastTemperature= ((0x7F & this->DHT_data[2]) *-100);
+    } else {
+      this->_lastTemperature= this->DHT_data[2]*100;
+    }
+    this->_lastHumidity= this->DHT_data[0]*100;
+  }
+  this->_lastTemperature = this->_lastTemperature/100;
+  this->_lastHumidity = this->_lastHumidity/100;
+
+  bRet= true;
   return bRet;
 }
 
