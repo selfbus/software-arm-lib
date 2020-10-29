b94aa81846fd471584cd2a32e0d573a3587fa583
olterion <o.stefan252@googlemail.com>
2020-10-29 19:21:31 +0100

    Update iaq-core.h

--- sblib/inc/sblib/i2c/iaq-core.h
+++ sblib/inc/sblib/i2c/iaq-core.h
@@ -1,3 +1,13 @@
+/*
+ * Library to connect a Air Quality Sensor IAQ-Core from AMS
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  Adapted to Selfbus Library by Oliver Stefan (2019)
+ */
+
 #ifndef IAQ_CORE_H
 #define IAQ_CORE_H
 
