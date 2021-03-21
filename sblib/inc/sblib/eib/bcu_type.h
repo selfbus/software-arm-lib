bd2a8979ff69af98089fae8d361a1d28eaa9d141
Darthyson <darth@maptrack.de>
2021-03-21 23:04:08 +0100

    first attempt to support BIM112 MaskVersion 0x0705, you can define "BIM112_75" to use it

--- sblib/inc/sblib/eib/bcu_type.h
+++ sblib/inc/sblib/eib/bcu_type.h
@@ -22,12 +22,6 @@
 #   define BCU_NAME BCU2
 #   define MASK_VERSION 0x20
 
-    /** Offset between the object index and the payload data
-     *  if the property commands are simulated via memory read/writes.
-     *  Only required for BIM112
-     */
-#   define PROP_LOAD_OFFSET 0
-
 #elif defined(BIM112) || defined(BIM112_71) || BCU_TYPE == 0x701
 //  BIM 112, mask version 7.1
 #   undef BCU_TYPE
@@ -35,15 +29,29 @@
 #   define BCU_NAME BIM112
 #   define MASK_VERSION 0x701
 
+// The BIM-112 type, for comparison
+#   define BIM112_TYPE 0x701
+
     /** Address for load control */
 #   define LOAD_CONTROL_ADDR 0x104
 
-    /** Offset between the object index and the payload data
-     *  if the property commands are simulated via memory read/writes
-     */
-#   define PROP_LOAD_OFFSET 1
+    /** Address for load state */
+#   define LOAD_STATE_ADDR 0xb6e9
 
-/** Address for load state */
+#elif defined(BIM112_75) || BCU_TYPE == 0x705
+//  BIM 112, mask version 7.5
+#   undef BCU_TYPE
+#   define BCU_TYPE 0x705
+#   define BCU_NAME BIM112_75
+#   define MASK_VERSION 0x705
+
+// The BIM-112 type, for comparison
+#   define BIM112_TYPE 0x705
+
+    /** Address for load control */
+#   define LOAD_CONTROL_ADDR 0x104
+
+    /** Address for load state */
 #   define LOAD_STATE_ADDR 0xb6e9
 
 
@@ -55,23 +63,13 @@
 #   define BCU_NAME BCU1
 #   define MASK_VERSION 0x12
 
-    /** Offset between the object index and the payload data
-     *  if the property commands are simulated via memory read/writes.
-     *  Only required for BIM112
-     */
-#   define PROP_LOAD_OFFSET 0
-
 #else
 //  Unknown BCU type
 #   error "Unknown BCU type. Please add a compiler define: either BCU_TYPE with the correct value or one of the predefined BCU types - see sblib/eib/bcu_type.h for valid types"
 #endif
 
-// The BCU 1 type, for comparison
+/** The BCU 1 type, for comparison between a BCU 1 or newer BCU's, do not change it */
 #define BCU1_TYPE 0x10
-
-// The BIM-112 type, for comparison
-#define BIM112_TYPE 0x701
-
 
 
 #if BCU_TYPE == 0x10  /* BCU 1 */
@@ -128,8 +126,39 @@
 #   define USER_EEPROM_START 0x3f00
 
     /** The size of the user EEPROM in bytes. */
-#   define USER_EEPROM_SIZE       3072
-#   define USER_EEPROM_FLASH_SIZE 4096
+#   ifndef __LPC11UXX__
+#       define USER_EEPROM_SIZE       3072
+#       define USER_EEPROM_FLASH_SIZE USER_EEPROM_SIZE
+#   else
+#       define USER_EEPROM_SIZE       3072  // was reduced from 4096 to 3072 to support LPC11Uxx. see commit 3194e058a850bdacb33cd068222647f1dbf19488
+#       define USER_EEPROM_FLASH_SIZE 4096
+#   endif
+
+#elif BCU_TYPE == 0x705  /* BIM 112, v7.5 */
+
+    /** Start address of the user RAM when ETS talks with us. */
+#   define USER_RAM_START_DEFAULT 0x5FC
+
+#		ifndef EXTRA_USER_RAM_SIZE
+#			define EXTRA_USER_RAM_SIZE 0
+#		endif
+    /** The size of the user RAM in bytes. */
+#   define USER_RAM_SIZE (0x304 + EXTRA_USER_RAM_SIZE)
+
+    /** How many bytes have to be allocated at the end of the RAM for shadowed values */
+#   define USER_RAM_SHADOW_SIZE 3
+
+    /** Start address of the user EEPROM when ETS talks with us. */
+#   define USER_EEPROM_START 0x3f00
+
+    /** The size of the user EEPROM in bytes. */
+#   ifndef __LPC11UXX__
+#       define USER_EEPROM_SIZE       3072
+#       define USER_EEPROM_FLASH_SIZE USER_EEPROM_SIZE
+#   else
+#       define USER_EEPROM_SIZE       3072  // was reduced from 4096 to 3072 to support LPC11Uxx. see commit 3194e058a850bdacb33cd068222647f1dbf19488
+#       define USER_EEPROM_FLASH_SIZE 4096
+#   endif
 
 #else
     // BCU_TYPE contains an invalid value and no other BCU type define is set
@@ -137,7 +166,7 @@
 #endif
 
 #ifndef USER_EEPROM_FLASH_SIZE
-#define USER_EEPROM_FLASH_SIZE USER_EEPROM_SIZE
+#   define USER_EEPROM_FLASH_SIZE USER_EEPROM_SIZE
 #endif
 
 /** End address of the user EEPROM +1, when ETS talks with us. */
