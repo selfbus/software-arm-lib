bf62be8500157f88e11b4db7dbf6b1704247ae6f
Darthyson <darth@maptrack.de>
2021-03-23 15:40:36 +0100

    moved "#defines's" which are the same for BIM MaskVersion 0x0701 and 0x0705 to one #ifdef section

--- sblib/inc/sblib/eib/bcu_type.h
+++ sblib/inc/sblib/eib/bcu_type.h
@@ -28,15 +28,7 @@
 #   define BCU_TYPE 0x701
 #   define BCU_NAME BIM112
 #   define MASK_VERSION 0x701
-
-// The BIM-112 type, for comparison
-#   define BIM112_TYPE 0x701
-
-    /** Address for load control */
-#   define LOAD_CONTROL_ADDR 0x104
-
-    /** Address for load state */
-#   define LOAD_STATE_ADDR 0xb6e9
+#   define BIM112_TYPE 0x701 //!> The BIM-112 type, for comparison between a BIM 112 7.1 and other BCU's, do not change it
 
 #elif defined(BIM112_75) || BCU_TYPE == 0x705
 //  BIM 112, mask version 7.5
@@ -44,16 +36,7 @@
 #   define BCU_TYPE 0x705
 #   define BCU_NAME BIM112_75
 #   define MASK_VERSION 0x705
-
-// The BIM-112 type, for comparison
-#   define BIM112_TYPE 0x705
-
-    /** Address for load control */
-#   define LOAD_CONTROL_ADDR 0x104
-
-    /** Address for load state */
-#   define LOAD_STATE_ADDR 0xb6e9
-
+#   define BIM112_TYPE 0x705  //!> The BIM-112 type, for comparison between a BIM 112 7.5 and other BCU's, do not change it
 
 #elif defined(BCU1) || defined(BCU1_12) || BCU_TYPE == 0x10 || BCU_TYPE == 10 || !defined(BCU_TYPE)
 //  BCU 1, mask version 1.2
@@ -68,9 +51,13 @@
 #   error "Unknown BCU type. Please add a compiler define: either BCU_TYPE with the correct value or one of the predefined BCU types - see sblib/eib/bcu_type.h for valid types"
 #endif
 
-/** The BCU 1 type, for comparison between a BCU 1 or newer BCU's, do not change it */
-#define BCU1_TYPE 0x10
 
+#define BCU1_TYPE 0x10 //!> The BCU 1 type, for comparison between a BCU 1 or newer BCU's, do not change it
+
+
+//
+// Set BCU_TYPE specific parameters, like UserRam, UserEeprom, LoadControladdress, LoadStateaddress
+//
 
 #if BCU_TYPE == 0x10  /* BCU 1 */
 
@@ -79,9 +66,9 @@
 
     /** The size of the user RAM in bytes. */
 #   define USER_RAM_SIZE 0x100
-	/** How many bytes have to be allocated at the end of the RAM
-		for shadowed values
-	*/
+    /** How many bytes have to be allocated at the end of the RAM
+        for shadowed values
+    */
 #   define USER_RAM_SHADOW_SIZE 3
 
     /** Start address of the user EEPROM when ETS talks with us. */
@@ -97,9 +84,9 @@
 
     /** The size of the user RAM in bytes. */
 #   define USER_RAM_SIZE 0x100
-	/** How many bytes have to be allocated at the end of the RAM
-		for shadowed values
-	*/
+    /** How many bytes have to be allocated at the end of the RAM
+        for shadowed values
+    */
 #   define USER_RAM_SHADOW_SIZE 0
 
     /** Start address of the user EEPROM when ETS talks with us. */
@@ -108,14 +95,21 @@
     /** The size of the user EEPROM in bytes. */
 #   define USER_EEPROM_SIZE 1024
 
-#elif BCU_TYPE == 0x701  /* BIM 112, v7.1 */
+#elif BCU_TYPE == 0x701 || BCU_TYPE == 0x705 /* BIM 112, v7.1 and v7.5*/
+
+    /** Address for load control */
+#   define LOAD_CONTROL_ADDR 0x104
+
+    /** Address for load state */
+#   define LOAD_STATE_ADDR 0xb6e9
 
     /** Start address of the user RAM when ETS talks with us. */
 #   define USER_RAM_START_DEFAULT 0x5FC
 
-#		ifndef EXTRA_USER_RAM_SIZE
-#			define EXTRA_USER_RAM_SIZE 0
-#		endif
+#   ifndef EXTRA_USER_RAM_SIZE
+#       define EXTRA_USER_RAM_SIZE 0
+#   endif
+
     /** The size of the user RAM in bytes. */
 #   define USER_RAM_SIZE (0x304 + EXTRA_USER_RAM_SIZE)
 
@@ -128,38 +122,11 @@
     /** The size of the user EEPROM in bytes. */
 #   ifndef __LPC11UXX__
 #       define USER_EEPROM_SIZE       3072
-#       define USER_EEPROM_FLASH_SIZE USER_EEPROM_SIZE
+#       define USER_EEPROM_FLASH_SIZE 4096
 #   else
 #       define USER_EEPROM_SIZE       3072  // was reduced from 4096 to 3072 to support LPC11Uxx. see commit 3194e058a850bdacb33cd068222647f1dbf19488
 #       define USER_EEPROM_FLASH_SIZE 4096
 #   endif
-
-#elif BCU_TYPE == 0x705  /* BIM 112, v7.5 */
-
-    /** Start address of the user RAM when ETS talks with us. */
-#   define USER_RAM_START_DEFAULT 0x5FC
-
-#		ifndef EXTRA_USER_RAM_SIZE
-#			define EXTRA_USER_RAM_SIZE 0
-#		endif
-    /** The size of the user RAM in bytes. */
-#   define USER_RAM_SIZE (0x304 + EXTRA_USER_RAM_SIZE)
-
-    /** How many bytes have to be allocated at the end of the RAM for shadowed values */
-#   define USER_RAM_SHADOW_SIZE 3
-
-    /** Start address of the user EEPROM when ETS talks with us. */
-#   define USER_EEPROM_START 0x3f00
-
-    /** The size of the user EEPROM in bytes. */
-#   ifndef __LPC11UXX__
-#       define USER_EEPROM_SIZE       3072
-#       define USER_EEPROM_FLASH_SIZE USER_EEPROM_SIZE
-#   else
-#       define USER_EEPROM_SIZE       3072  // was reduced from 4096 to 3072 to support LPC11Uxx. see commit 3194e058a850bdacb33cd068222647f1dbf19488
-#       define USER_EEPROM_FLASH_SIZE 4096
-#   endif
-
 #else
     // BCU_TYPE contains an invalid value and no other BCU type define is set
 #   error "Unsupported BCU type"
