5cb6823ffde89d8c3b477690bc91cd0f5d59e6cf
Mirko Jelinek <mirko.jelinek@telekom.de>
2021-10-02 20:11:41 +0200

    delay() optimized

--- sblib/src/timer.cpp
+++ sblib/src/timer.cpp
@@ -27,11 +27,20 @@ void delay(unsigned int msec)
     // otherwise "while" will end in an infinite loop
     if (__get_IPSR() != 0x0)
     {
-        while (msec)
-        {
-            delayMicroseconds(1000);
-            --msec;
-        }
+    	while (msec)
+    	{
+    		if (msec > MAX_DELAY_MILLISECONDS)
+			{
+    			delayMicroseconds(MAX_DELAY_MILLISECONDS * 1000);
+				msec -= (MAX_DELAY_MILLISECONDS);
+			}
+			else
+			{
+				delayMicroseconds(msec * 1000);
+				msec = 0;
+			}
+    	}
+
         return;
     }
 #endif
