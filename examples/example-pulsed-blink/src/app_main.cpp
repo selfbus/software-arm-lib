e27fbe96759f126fdec0dd1a0afb64a7e0f2a6de
Darthyson <darth@maptrack.de>
2021-10-04 23:39:34 +0200

    updated example-pulsed-blink to recent sblib version

--- examples/example-pulsed-blink/src/app_main.cpp
+++ examples/example-pulsed-blink/src/app_main.cpp
@@ -1,26 +1,42 @@
+/**************************************************************************//**
+ * @file    app_main.cpp
+ * @brief   A simple application that sends pulses on a digital port.
+ *
+ * By default PIO0.7 is used, which is the LED on the LPCxpresso board.
+ * needs BCU1 version of the sblib library.
+ *
+ * This example is meant to be a test for the timing of delayMicroseconds().
+ *
+ * @author Stefan Taferner <stefan.taferner@gmx.at> Copyright (c) 2015
+ * @author Darthyson <darth@maptrack.de> Copyright (c) 2021
+ * @bug No known bugs.
+ ******************************************************************************/
+
 /*
- *  A simple application that sends pulses on a digital port. Per default
- *  PIO0_7 is used, which is the LED on the LPCxpresso board.
- *
- *  This example is meant to be a test for the timing of delayMicroseconds().
- *
- *  Copyright (c) 2015 Stefan Taferner <stefan.taferner@gmx.at>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3 as
- *  published by the Free Software Foundation.
- */
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 3 as
+ published by the Free Software Foundation.
+ ---------------------------------------------------------------------------*/
 
 #include <sblib/core.h>
+#include <sblib/eib/sblib_default_objects.h>
+#include <sblib/digital_pin.h>
+#include <sblib/io_pin_names.h>
 
-// The pin that is used for the example
+/**
+ * The pin that is used for this example, see sblib/io_pin_names.h for other options
+ * e.g. PIN_IO1, PIN_RUN, PIN_INFO
+ */
 int pin = PIO0_7;
+// int pin = PIN_IO1;
+// int pin = PIN_RUN;
+// int pin = PIN_INFO;
 
 int mask = digitalPinToBitMask(pin);
 LPC_GPIO_TypeDef* port = gpioPorts[digitalPinToPort(pin)];
 
 
-/*
+/**
  * Initialize the application.
  */
 void setup()
@@ -28,10 +44,10 @@ void setup()
     pinMode(pin, OUTPUT);
 }
 
-/*
- * The main processing loop.
+/**
+ * The main processing loop while no KNX-application is loaded.
  */
-void loop()
+void loop_noapp()
 {
     /* To be as accurate as possible, we directly access the IO pin and do not
      * use digitalWrite().
@@ -63,4 +79,12 @@ void loop()
     delayMicroseconds(100000);
     port->MASKED_ACCESS[mask] = 0;
     delay(500);
+}
+
+/**
+ * The main processing loop.
+ */
+void loop()
+{
+    // will never be called in this example
 }
