f348a5fe7cf16e1e07f66395b4f4977e35dd612c
Darthyson <darth@maptrack.de>
2021-10-05 13:43:43 +0200

    added possibility to change SystemCoreClock and output of system clock on ISP-Enable pin PIO0.1 - Use connectSystemOscillatorToClkOutPin() with caution, as this may break debugging. In that case you can revive it by flashing with Flashmagic.

--- examples/example-pulsed-blink/src/app_main.cpp
+++ examples/example-pulsed-blink/src/app_main.cpp
@@ -45,6 +45,8 @@
 ///< macro to set the test pin low
 #define SET_TESTPIN_LOW port->MASKED_ACCESS[mask] = 0
 
+enum SystemSpeed {mhz12, mhz24, mhz36, mhz48};
+
 /**
  * The pin that is used for this example, see sblib/io_pin_names.h for other options
  * e.g. PIN_IO1, PIN_RUN, PIN_INFO
@@ -61,17 +63,78 @@ int mask = digitalPinToBitMask(testPin);
 LPC_GPIO_TypeDef* port = gpioPorts[digitalPinToPort(testPin)];
 
 /**
+ * @fn void connectSystemOscillatorToClkOutPin()
+ * @brief Connect the system oscillator to clock-out pin (PIO0.1)
+ *        This allows measurement with oscilloscope.
+ *
+ *        USE WITH CAUTION !
+ *        Trying to debug may run into error 'Ee(42). Could not connect to core.'.
+ *        This is due to CLKOUT and ISP_Enable share the same pin.
+ *        Use Flashmagic to restore the LPC.
+ */
+void connectSystemOscillatorToClkOutPin()
+{
+    LPC_IOCON->PIO0_1        &= 0xFFFFFFF8; //clear FUNC bits (sec. 7.4.4)
+    LPC_IOCON->PIO0_1        |= 0x01;       //set FUNC bits to CLKOUT function (sec. 7.4.4)
+    LPC_GPIO0->DIR           |= 0x01;       //set CLKOUT pin to output (sec. 12.3.2)
+    LPC_SYSCON->CLKOUTCLKSEL = 0x3;         //sec. 3.5.21
+    LPC_SYSCON->CLKOUTUEN    = 0;           //"CLKOUTUEN" has to be toggled for "CLKOTUCLKSEL" to accept the value written to it
+    LPC_SYSCON->CLKOUTUEN    = 1;           //"CLKOUTUEN" has to be toggled for "CLKOTUCLKSEL" to accept the value written to it
+    LPC_SYSCON->CLKOUTDIV    = 1;           //set divider to 1 (sec. 3.5.23)
+}
+
+/**
+ * @fn void setSystemSpeed(SystemSpeed)
+ * @brief Changes SystemCoreClock to newSystemSpeed
+ *
+ * @param[in] newSystemSpeed
+ */
+void setSystemSpeed(SystemSpeed newSystemSpeed)
+{
+    unsigned int msel;
+    unsigned int psel;
+
+    switch (newSystemSpeed)
+    {
+        case mhz12 :
+            msel = 0x0;
+            psel = 0x0;
+            break;
+        case mhz24 :
+            msel = 0x1;
+            psel = 0x2;
+            break;
+        case mhz36 :
+            msel = 0x2;
+            psel = 0x2;
+            break;
+        default: // 48MHz
+            msel = 0x3;
+            psel = 0x1;
+            break;
+    }
+
+    LPC_SYSCON->SYSPLLCTRL   = (psel << 5) | msel;
+    LPC_SYSCON->SYSPLLCLKUEN = 0;               // toggle PLL update register
+    LPC_SYSCON->SYSPLLCLKUEN = 1;
+    while (!(LPC_SYSCON->SYSPLLSTAT & 0x01));   // wait for update to be finished
+    SystemCoreClockUpdate();
+    SysTick_Config(SystemCoreClock / 1000);
+}
+
+/**
  * Initialize the application.
  */
 void setup()
 {
     pinMode(testPin, OUTPUT);
+    // setSystemSpeed(mhz48); // test different SystemCoreClocks
+    connectSystemOscillatorToClkOutPin();
 }
 
 /**
- * delayMicroseconds() method.
+ * Test of delayMicroseconds() method.
  */
-
 void test_delayMicroseconds()
 {
     /* To be as accurate as possible, we directly access the IO pin and do not
