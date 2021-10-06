d8d9f5a89c577b26e61b4c18ea0d16631dbe44a7
Darthyson <darth@maptrack.de>
2021-10-06 03:13:24 +0200

    updated to recent sblib version

--- examples/example-graphlcd/src/app_main.cpp
+++ examples/example-graphlcd/src/app_main.cpp
@@ -1,45 +1,48 @@
+/**************************************************************************//**
+ * @file    app_main.cpp
+ * @brief   An example application for a graphical LCD.
+ *          We use the EA DOGS 102, a 102x64 monochrome graphical LCD.
+ *
+ *          We use SPI port 0 in this example.
+ *
+ *          Connect the ARM in this way to the EA-DOGS:
+ *
+ *          PIO0_2:  SSEL0 -> Display CS0 "chip select"
+ *          PIO0_9:  MOSI0 -> Display SDA "data in"
+ *          PIO2_11: SCK0  -> Display SCK "clock"
+ *          PIO0_8:        -> Display CD  "command/data"
+ *
+ *          needs BCU1 version of the sblib library
+ *
+ * @author Stefan Taferner <stefan.taferner@gmx.at> Copyright (c) 2014
+ * @author Darthyson <darth@maptrack.de> Copyright (c) 2021
+ * @bug No known bugs.
+ ******************************************************************************/
+
 /*
- *  An example application for a graphical LCD.
- *  We use the EA DOGS 102, a 102x64 monochrome graphical LCD.
- *
- *  Copyright (c) 2014 Stefan Taferner <stefan.taferner@gmx.at>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3 as
- *  published by the Free Software Foundation.
- */
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 3 as
+ published by the Free Software Foundation.
+ ---------------------------------------------------------------------------*/
 
 #include <sblib/lcd/graphical_eadogs.h>
 #include <sblib/lcd/font_5x7.h>
 #include <sblib/core.h>
 #include <sblib/eib/sblib_default_objects.h>
 #include <sblib/ioports.h>
+#include <sblib/io_pin_names.h>
 
-
-/*
- * We use SPI port 0 in this example.
- *
- * Connect the ARM in this way to the EA-DOGS:
- *
- * PIO0_2:  SSEL0 -> Display CS0 "chip select"
- * PIO0_9:  MOSI0 -> Display SDA "data in"
- * PIO2_11: SCK0  -> Display SCK "clock"
- * PIO0_8:        -> Display CD  "command/data"
- */
-
+#define BLINK_PIN PIN_IO2
 
 LcdGraphicalEADOGS display(SPI_PORT_0, PIO0_9, PIO2_11, PIO0_8, PIO0_2, font_5x7);
 
-int blinkPin = PIO0_7;
-
-
-/*
+/**
  * Initialize the application.
  */
 void setup()
 {
     display.begin();
-    pinMode(blinkPin, OUTPUT);
+    pinMode(BLINK_PIN, OUTPUT);
 
     display.clear();
     display.println("********************");
@@ -49,11 +52,19 @@ void setup()
     display.println("********************");
 }
 
-/*
+/**
+ * The main processing loop while no KNX-application is loaded.
+ */
+void loop_noapp()
+{
+    digitalWrite(BLINK_PIN, !digitalRead(BLINK_PIN));
+    delay(1000);
+}
+
+/**
  * The main processing loop.
  */
 void loop()
 {
-    digitalWrite(blinkPin, !digitalRead(blinkPin));
-    delay(1000);
+    // will never be called in this example
 }
