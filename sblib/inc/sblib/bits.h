b22d581475289dfa40e4531e9cd327eea69a3716
Thomas Dallmair <dev@thomas-dallmair.de>
2023-12-27 19:46:50 +0100

    [sblib] Remove unused bit twiddling macros from bits.h

--- sblib/inc/sblib/bits.h
+++ sblib/inc/sblib/bits.h
@@ -16,50 +16,6 @@
 
 
 /**
- * Compute the value of the specified bit.
- *
- * @param bitno - the number of the bit (0, 1, 2... 31)
- */
-#define bit(bitno) (1UL << (bitno))
-
-/**
- * Clear the bit of a number. The number can be
- * any integer (byte, short, int, long).
- *
- * @param val - the number from which to clear the bit
- * @param bitno - the number of the bit (0, 1, 2... 31)
- */
-#define bitClear(val, bitno) ((val) &= ~(1UL << (bitno)))
-
-/**
- * Set the bit of a number. The number can be
- * any integer (byte, short, int, long).
- *
- * @param val - the number from which to set the bit
- * @param bitno - the number of the bit (0, 1, 2... 31)
- */
-#define bitSet(val, bitno) ((val) |= 1UL << (bitno))
-
-/**
- * Write the value of a bit of a number.
- *
- * @param val - the number from which to write the bit
- * @param bitno - the number of the bit (0, 1, 2... 31)
- * @param b - the bit value (0 or 1)
- */
-#define bitWrite(val, bitno, b) ((b) ? bitSet(val, bitNo) : bitClear(val, bitNo))
-
-/**
- * Read the value of a bit of a number. The number can be
- * any integer (byte, short, int, long).
- *
- * @param val - the number from which to get the bit
- * @param bitno - the number of the bit (0, 1, 2... 31)
- * @return The value of the bit (0 or 1).
- */
-#define bitRead(val, bitno) (((val) >> (bitno)) & 1)
-
-/**
  * Extract the lowest (rightmost) byte of a number. The number can be
  * any integer (byte, short, int, long).
  *
