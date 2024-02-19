selfbus/software-arm-lib

commit 229bc36b6a69f15a56c34a869f394dbdcc43ad02

Schedule last repetition as early as possible
Implement bus access fairness for first transmission and all
repetitions, except for the last one. For the last repetition it makes
sense to send it out as early as possible to reduce the risk of
inadvertently losing (dropping) the telegram.

sblib/src/eib/bus.cpp
@@ -877,8 +877,16 @@ __attribute__((optimize("Os"))) void Bus::timerInterruptHandler()
             else
                 time = PRE_SEND_TIME;
             // KNX spec 2.1 chapter 3/2/2 section 2.3.4 p. 37: Guarantee of access fairness
-            // Add some random delay of up to 3 bit times.
-            time += (millis() * RANDOMIZE_FACTOR) % RANDOMIZE_MODULUS;
+            // Add some random delay of up to 3 bit times if it is not the last try.
+            if (sendRetriesMax > 0 && sendBusyRetriesMax > 0 && // Repetitions allowed and
+                (!repeatTelegram ||                             // either it's the first transmission, or
+                    (sendRetries + 1 < sendRetriesMax && sendBusyRetries + 1 < sendBusyRetriesMax && // not the last repetition
+                    collisions < COLLISION_RETRY_MAX)
+                )
+               )
+            {
+                time += (millis() * RANDOMIZE_FACTOR) % RANDOMIZE_MODULUS;
+            }
             //tb_d( state+ 300, time, tb_in);
             //tb_h( state+ 400,sendCurTelegram[0], tb_in);
         }

commit a2bb9b3e3f1eedd0cc27012588d9218b9027559d

Fix condition to detect last transmission chance (bus access fairness)
One parenthesis was wrong in the original code introduced in commit
229bc36b6a69f15a56c34a869f394dbdcc43ad02. Writing the code in a better
readable way makes it way easier to understand.

This caused the last transmission to not be timed as early as possible
on a highly loaded bus, with logs showing more than four consecutive
collisions while trying to send out a `SEND_LAST_ERROR` telegram.

sblib/src/eib/bus.cpp
@@ -892,12 +892,10 @@ __attribute__((optimize("Os"))) void Bus::timerInterruptHandler()
                 time = PRE_SEND_TIME;
             // KNX spec 2.1 chapter 3/2/2 section 2.3.4 p. 37: Guarantee of access fairness
             // Add some random delay of up to 3 bit times if it is not the last try.
-            if (sendRetriesMax > 0 && sendBusyRetriesMax > 0 && // Repetitions allowed and
-                (!repeatTelegram ||                             // either it's the first transmission, or
-                    (sendRetries + 1 < sendRetriesMax && sendBusyRetries + 1 < sendBusyRetriesMax && // not the last repetition
-                    collisions < COLLISION_RETRY_MAX)
-                )
-               )
+            auto canRepeat = sendRetriesMax > 0 && sendBusyRetriesMax > 0;
+            auto isLastRepeatChance = repeatTelegram && (sendRetries + 1 >= sendRetriesMax || sendBusyRetries + 1 >= sendBusyRetriesMax);
+            auto isLastCollisionChance = collisions == COLLISION_RETRY_MAX;
+            if (canRepeat && !isLastRepeatChance && !isLastCollisionChance)
             {
                 time += (millis() * RANDOMIZE_FACTOR) % RANDOMIZE_MODULUS;
             }