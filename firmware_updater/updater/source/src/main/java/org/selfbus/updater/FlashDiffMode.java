110ece00513cb33214ff504c3d70624b780ea05f
Darthyson <darth@maptrack.de>
2024-03-06 14:05:08 +0100

    [Updater] Drop version in .bin cache directory path, closes #99
    There is no reason to organize the *.bin files in subfolders corresponding to the version of the updater. A *.bin stays a *.bin no matter what.

--- firmware_updater/updater/source/src/main/java/org/selfbus/updater/FlashDiffMode.java
+++ firmware_updater/updater/source/src/main/java/org/selfbus/updater/FlashDiffMode.java
@@ -25,10 +25,10 @@ import static org.selfbus.updater.Mcu.MAX_PAYLOAD;
 public final class FlashDiffMode {
     private final static Logger logger = LoggerFactory.getLogger(FlashDiffMode.class.getName());
     // hexCacheDir will be used as a cache directory for the cached *.hex files used by the differential update mode
-    // windows: C:\Users\[currentUser]\AppData\Local\Selfbus\Selfbus-Updater\Cache\[version]
-    // linux  : /home/[user-home]]/.cache/Selfbus-Updater/[version]
+    // windows: C:\Users\[currentUser]\AppData\Local\Selfbus\Selfbus-Updater\Cache\
+    // linux  : /home/[user-home]]/.cache/Selfbus-Updater/
     private static final String hexCacheDirectory = AppDirsFactory.getInstance().getUserCacheDir(
-            ToolInfo.getTool(), ToolInfo.getVersion(), ToolInfo.getAuthor());
+            ToolInfo.getTool(), "", ToolInfo.getAuthor());
     private final static String IMAGE_IDENTIFIER = "image";
     private final static String FILE_NAME_SEP = "-";
     private final static String FILE_EXTENSION = ".bin";
