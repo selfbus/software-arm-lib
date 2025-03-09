5adc00e19e7bb1593e2b41cab671f7be22407fe7
Darthyson <darth@maptrack.de>
2025-03-09 16:36:49 +0100

    [Updater] Revert `Filter eibd hop count decrement info message`
    This commit reverts partially the commit 2d918cc.
    Just use knxd if this info message bothers you.

--- firmware_updater/updater/src/org/selfbus/updater/logging/MessageFilter.java
+++ firmware_updater/updater/src/org/selfbus/updater/logging/MessageFilter.java
@@ -23,10 +23,7 @@ public class MessageFilter extends Filter<ILoggingEvent> {
      */
     public static final List<String> FILTERED_MESSAGES = List.of(
             // Loxone Miniserver Gen1 returns always status 0x04 on KNX tunnel requests with sequence number 255
-            "received service acknowledgment status sequence number out of order",
-            // from calimero-core KNXnetIPTunnel.java:
-            // we could get a .con with its hop count already decremented by 1 (eibd does that)
-            "received L_Data.con with hop count decremented by 1 (sent"
+            "received service acknowledgment status sequence number out of order"
     );
 
     /**
